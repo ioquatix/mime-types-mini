8cf311c191911328aeb94327c9e1a4afa97a66be
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-10-10 01:40:09 +1300

    Add support for content-type lookup.

---
+++ lib/mime/types.rb
@@ -0,0 +1,18 @@
+
+module MIME
+	module Types
+		def self.type_for(filename)
+			# Return mime types
+		end
+		
+		def self.[](content_type)
+			# Return mime types
+		end
+		
+		def self.content_type_for(extension)
+		end
+		
+		def self.extensions_for(content_type)
+		end
+	end
+end
--- lib/mime/types/mini/version.rb
+++ lib/mime/types/mini/version.rb
@@ -21,7 +21,7 @@
 module Mime
 	module Types
 		module Mini
-			VERSION = "0.1.0"
+			VERSION = "0.2.0"
 		end
 	end
 end
